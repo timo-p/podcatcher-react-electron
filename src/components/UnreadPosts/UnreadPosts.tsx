776e48ec5fcdf9b1d6e76f7100cfc70597db08f3
Timo Paananen <tpaananen@gmail.com>
2021-04-05 09:14:44 +0300

    Add show all functionality to unread posts view

--- src/components/UnreadPosts/UnreadPosts.tsx
+++ src/components/UnreadPosts/UnreadPosts.tsx
@@ -1,8 +1,16 @@
-import { Box, IconButton, Typography } from '@material-ui/core';
+import {
+  Box,
+  Checkbox,
+  FormControlLabel,
+  IconButton,
+  Typography,
+} from '@material-ui/core';
 import { Done } from '@material-ui/icons';
+import { sortBy } from 'ramda';
 import React from 'react';
 import { StateFeed } from '../../reducers/types';
 import { Post as PostType } from '../../types/types';
+import filterUnreadPosts from '../../utils/filterUnreadPosts';
 import Posts from '../Posts/Posts';
 import styles from './UnreadPosts.module.css';
 
@@ -17,22 +25,38 @@ export default function UnreadPosts({
   feeds,
   markPostsRead,
 }: UnreadPostsProps) {
+  const [showAll, setShowAll] = React.useState(false);
+
+  const showPosts = sortBy(
+    (post) => new Date(post.pubDate),
+    showAll ? posts : filterUnreadPosts(posts)
+  ).reverse();
+
   return (
     <>
       <Box className={styles.topBox}>
         <Typography variant="h4">Unread posts</Typography>
-        {posts.length > 0 && (
-          <Typography>{posts.length} unread posts.</Typography>
+        {showPosts.length > 0 && (
+          <Typography>
+            {showPosts.length} {!showAll && 'unread '}post
+            {showPosts.length !== 1 && 's'}.
+          </Typography>
         )}
 
-        {posts.length === 0 && <Typography>No unread posts</Typography>}
-        {posts.length > 0 && (
-          <IconButton onClick={() => markPostsRead(posts.map((p) => p.id))}>
+        {showPosts.length === 0 && <Typography>No unread posts</Typography>}
+        <FormControlLabel
+          control={
+            <Checkbox checked={showAll} onChange={() => setShowAll(!showAll)} />
+          }
+          label="Show all posts"
+        />
+        {showPosts.length > 0 && (
+          <IconButton onClick={() => markPostsRead(showPosts.map((p) => p.id))}>
             <Done />
           </IconButton>
         )}
       </Box>
-      {posts.length > 0 && <Posts posts={posts} feeds={feeds} />}
+      {showPosts.length > 0 && <Posts posts={showPosts} feeds={feeds} />}
     </>
   );
 }
--- src/containers/UnreadPosts.tsx
+++ src/containers/UnreadPosts.tsx
@@ -1,20 +1,16 @@
 import { push } from 'connected-react-router';
-import { sortBy, values } from 'ramda';
+import { values } from 'ramda';
 import { connect } from 'react-redux';
 import { bindActionCreators, Dispatch } from 'redux';
 import { removeFeed, markPostsRead } from '../actions';
 import UnreadPosts from '../components/UnreadPosts/UnreadPosts';
 import { PodcatcherStateType } from '../reducers/types';
-import filterUnreadPosts from '../utils/filterUnreadPosts';
 
 function mapStateToProps(state: PodcatcherStateType) {
-  const posts = sortBy(
-    (post) => new Date(post.pubDate),
-    filterUnreadPosts(values(state.posts))
-  ).reverse();
+  const { posts, feeds } = state;
   return {
-    posts,
-    feeds: state.feeds,
+    posts: values(posts),
+    feeds,
   };
 }
 
